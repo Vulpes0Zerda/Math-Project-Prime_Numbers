dec0e1e21739da4372d5786ad949b0d356dd9cc4
Lucy B. <lucyburock@gmail.com>
2025-07-15 11:22:52 +0200

    added test cases

--- src/test/java/math/primenumbers/PrimeNumberTest.java
+++ src/test/java/math/primenumbers/PrimeNumberTest.java
@@ -2,38 +2,45 @@ package math.primenumbers;
 
 
 import org.junit.jupiter.api.Test;
-import math.primenumbers.PrimeNumberUtils.isPrime;
-import math.primenumbers.PrimeNumberUtils.countPrimes;
 
 public class PrimeNumberTest {
   @Test
   public void testIsPrime() {
-    assert isPrime(2);
-    assert isPrime(3);
-    assert isPrime(5);
-    assert isPrime(7);
-    assert isPrime(11);
-    assert isPrime(13);
-    assert isPrime(17);
-    assert isPrime(19);
-    assert isPrime(23);
-    assert isPrime(29);
+    assert PrimeNumber.isPrime(2);
+    assert PrimeNumber.isPrime(3);
+    assert PrimeNumber.isPrime(5);
+    assert PrimeNumber.isPrime(7);
+    assert PrimeNumber.isPrime(11);
+    assert PrimeNumber.isPrime(13);
+    assert PrimeNumber.isPrime(17);
+    assert PrimeNumber.isPrime(19);
+    assert PrimeNumber.isPrime(23);
+    assert PrimeNumber.isPrime(29);
 
-    assert !isPrime(1);
-    assert !isPrime(4);
-    assert !isPrime(6);
-    assert !isPrime(8);
-    assert !isPrime(9);
-    assert !isPrime(10);
+    assert !PrimeNumber.isPrime(1);
+    assert !PrimeNumber.isPrime(4);
+    assert !PrimeNumber.isPrime(6);
+    assert !PrimeNumber.isPrime(8);
+    assert !PrimeNumber.isPrime(9);
+    assert !PrimeNumber.isPrime(10);
   }
 
   public void testCountPrimes() {
-    assert countPrimes(10) == 4;
-    assert countPrimes(20) == 8;
-    assert countPrimes(30) == 10;
-    assert countPrimes(100000) == 9592;
-    assert countPrimes(1) == 0;
-    assert countPrimes(0) == 0;
+    assert PrimeNumber.countPrimes(10) == 4;
+    assert PrimeNumber.countPrimes(20) == 8;
+    assert PrimeNumber.countPrimes(30) == 10;
+    assert PrimeNumber.countPrimes(100000) == 9592;
+    assert PrimeNumber.countPrimes(1) == 0;
+    assert PrimeNumber.countPrimes(0) == 0;
+  }
+
+  public void testEratosthenes() {
+    assert PrimeNumber.countArray(PrimeNumber.eratosthenes(10), true) == 4;
+    assert PrimeNumber.countArray(PrimeNumber.eratosthenes(20), true) == 8;
+    assert PrimeNumber.countArray(PrimeNumber.eratosthenes(30), true) == 10;
+    assert PrimeNumber.countArray(PrimeNumber.eratosthenes(100000), true)  == 9592;
+    assert PrimeNumber.countArray(PrimeNumber.eratosthenes(1), true) == 0;
+    assert PrimeNumber.countArray(PrimeNumber.eratosthenes(0), true) == 0;
   }
 
 }
